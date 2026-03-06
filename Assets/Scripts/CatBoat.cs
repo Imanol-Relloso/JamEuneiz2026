eca554ce65cbd740e01aaba91db1fcaeb33718bc
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-03-06 13:55:22 +0100

    Update: Gatobarco

--- Assets/Scripts/CatBoat.cs
+++ Assets/Scripts/CatBoat.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
-public class Cat : MonoBehaviour
+public class CatBoat : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Errores")]
+    [Tooltip("Error de nombre en el dialogo")]
+    [SerializeField] private bool ErrorEnNombre;
+    [Tooltip("Error de pais en el dialogo")]
+    [SerializeField] private bool ErrorEnPais;
+    [Tooltip("Error de pais en la bandera")]
+    [SerializeField] private bool ErrorEnBandera;
+    [Tooltip("Error de carga en el dialogo")]
+    [SerializeField] private bool ErrorEnCarga;
+
+    private NameSystem nameSystem;
+    private CountrySystem countrySystem;
+    private LoadSystem loadSystem;
+
+    private void Start()
     {
-        
+        Generate();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Generate()
     {
-        
+        nameSystem = new NameSystem();
+        nameSystem.SetValues(ErrorEnNombre);
+
+        countrySystem = new CountrySystem();
+        countrySystem.SetValues(ErrorEnBandera,ErrorEnPais);
+
+        loadSystem = new LoadSystem();
+        loadSystem.SetValues(ErrorEnCarga);
+    }
+
+    public bool IsValid()
+    {
+        return
+            nameSystem.IsCorrect() &&
+            countrySystem.IsCorrect() &&
+            loadSystem.IsCorrect();
     }
 }
