60b7b81e268e447249e57e36a01d87ca1c06b006
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-03-09 11:06:07 +0100

    Add: diccionario de enums por sprites

---
+++ Assets/Scripts/SpritesDictionary/EnumSprite.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[System.Serializable]
+public class NameSprite
+{
+    public Name name;
+    public Sprite sprite;
+}
+[System.Serializable]
+public class CountrySprite
+{
+    public Country country;
+    public Sprite sprite;
+}
+[System.Serializable]
+public class LoadSprite
+{
+    public Load Load;
+    public Sprite sprite;
+}
---
+++ Assets/Scripts/SpritesDictionary/SpriteDictionary.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SpriteDictionary : MonoBehaviour
+{
+    public static SpriteDictionary Instance;
+
+    [Header("NAME SPRITES")]
+    [SerializeField] private NameSprite[] nameSprites;
+    [Header("COUNTRY SPRITES")]
+    [SerializeField] private CountrySprite[] countrySprites;
+    [Header("LOAD SPRITES")]
+    [SerializeField] private LoadSprite[] loadSprites;
+
+    private static Dictionary<Name, Sprite> nameDict;
+    private static Dictionary<Country, Sprite> countryDict;
+    private static Dictionary<Load, Sprite> loadDict;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        InitializeDict();
+    }
+
+    private void InitializeDict()
+    {
+        nameDict = new Dictionary<Name, Sprite>();
+        countryDict = new Dictionary<Country, Sprite>();
+        loadDict = new Dictionary<Load, Sprite>();
+
+        foreach (NameSprite nameSprite in nameSprites)
+            nameDict[nameSprite.name] = nameSprite.sprite;
+        foreach (CountrySprite countrySprite in countrySprites)
+            countryDict[countrySprite.country] = countrySprite.sprite;
+        foreach (LoadSprite loadSprite in loadSprites)
+            loadDict[loadSprite.Load] = loadSprite.sprite;
+    }
+
+    public static Sprite GetNameSprite(Name name)
+    {
+        return nameDict[name];
+    }
+    public static Sprite GetCountrySprite(Country country)
+    {
+        return countryDict[country];
+    }
+    public static Sprite GetLoadSprite(Load load)
+    {
+        return loadDict[load];
+    }
+}
