16996d0110b22b9906d3874e677e312057a3c419
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-03-14 17:31:04 +0100

    fix: current day a public

--- Assets/Scripts/Days/DayManager.cs
+++ Assets/Scripts/Days/DayManager.cs
@@ -12,7 +12,7 @@ public class DayManager : MonoBehaviour
 {
     public static DayManager Instance;
 
-    [SerializeField] private int currentDay;
+    [SerializeField] public int currentDay;
 
     [Header("PREFABS DE LOS DIAS")]
     [SerializeField] private Day[] days;
